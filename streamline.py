892f80d7702562dd467369ca520c15f15729aa87
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-25 20:51:45 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import numpy as np
 st.title ("2022 World Cup Stats")
-new_title = '<p style="font-family:sans-serif; color:Light Blue; font-size: 42px;">2022 World Cup Stats</p>'
-st.markdown(new_title, unsafe_allow_html=True)
 st.header("An analysis on the 2022 world cup")
 st.image("messi.jpg")
+
+st.radio('Pick your favorite team',['Argentina','Brazil','France','Germany','Morocco','Spain'])
+st.selectbox('Pick your sportr',['Basketball','Football'])
+st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+
 
 
