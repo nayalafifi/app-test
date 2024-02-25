67380fb15822e20e8c7c6fd2fe03e25cf045ca70
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-25 20:32:38 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -1,3 +1,10 @@
+%%writefile app.py
+
 import streamlit as st
-st.write('Hello World')
-st.write('Run Streamlit on Colab')
+st.title ("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
