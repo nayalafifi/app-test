088dcfce9eb0ab4e3b10f5b4c18179711396e57f
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-25 20:47:07 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -2,10 +2,9 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 st.title ("2022 World Cup Stats")
+new_title = '<p style="font-family:Arial; color:Blue; font-size: 42px;">2022 World Cup Stats</p>'
+st.markdown(new_title, unsafe_allow_html=True)
 st.header("An analysis on the 2022 world cup")
 st.image("messi.jpg")
 
-rand=np.random.normal(1, 2, size=20)
-fig, ax = plt.subplots()
-ax.hist(rand, bins=15)
-st.pyplot(fig)
+
