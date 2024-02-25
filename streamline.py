1f39fb4172b00e794179e9ee657778f3e17bf0cf
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-25 20:30:20 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -1,7 +1,7 @@
 !pip install streamlit --quiet
 !pip install pyngrok --quiet
 
-%%writefile app.py
+%%writefile streamline.py
 import streamlit as st
 st.write('# Hello World')
 st.write('## Run Streamlit on Colab')
