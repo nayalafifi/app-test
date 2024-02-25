900116b6ab567676c7e6f59e8f1309d727d4002b
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-26 00:29:09 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -20,46 +20,28 @@ fig, ax = plt.subplots()
 sns.distplot(df['popularity'], ax=ax)
 st.pyplot(fig)
 
-# # Correlation Heatmap of Audio Features
+# Correlation Heatmap of Audio Features
 audio_features = df[['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo', 'valence']]
 audio_correlation = audio_features.corr()
-# plt.figure(figsize=(12, 8))
-# sns.heatmap(audio_correlation, annot=True, cmap='coolwarm', fmt=".2f")
-# plt.title('Correlation Matrix of Audio Features')
-# plt.show()
 plt.figure(figsize=(12, 8))
 sns.heatmap(audio_correlation, annot=True, cmap='coolwarm', fmt=".2f")
 plt.title('Correlation Matrix of Audio Features')
-st.pyplot()  # This will display the heatmap
+st.pyplot() 
+
 # Top Most Popular Albums
 top_albums = df.groupby('album')['popularity'].mean().sort_values(ascending=False).head(10)
 
 # Reverse the order of the DataFrame
 top_albums = top_albums[::-1]
 
-top_albums.plot(kind='barh', color='skyblue') 
+fig, ax = plt.subplots()
+top_albums.plot(kind='barh', color='skyblue', ax=ax) 
 plt.title('Top Albums by Popularity')
 plt.xlabel('Average Popularity')  
 plt.ylabel('Album')  
-plt.show()
+st.pyplot(fig)
 
 
 
 
 
-
-
-# import altair as alt
-
-# df = pd.DataFrame(
-#    np.random.randn(500, 3),
-#    columns=['x','y','z'])
-
-# c = alt.Chart(df).mark_circle().encode(
-#    x='x' , y='y' , size='z', color='z', tooltip=['x', 'y', 'z'])
-# st.altair_chart(c, use_container_width=True)
-
-# import seaborn as sns
-# sampled_df = df.sample(n=1000)
-# sampled_df_10_columns = sampled_df.iloc[:, :10]
-# sns.pairplot(sampled_df_10_columns)
