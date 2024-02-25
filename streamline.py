260055777cabb0cc64322b3ef8f402bc16d55d59
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-25 20:47:29 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import numpy as np
 st.title ("2022 World Cup Stats")
 new_title = '<p style="font-family:Arial; color:Blue; font-size: 42px;">2022 World Cup Stats</p>'
