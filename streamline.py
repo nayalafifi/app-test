99334572cb89a0500f30b0338163f8fabcd210b7
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-25 20:36:46 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -1,9 +1,8 @@
-
 import streamlit as st
-st.title ("this is the app title")
-st.header("this is the markdown")
-st.markdown("this is the header")
-st.subheader("this is the subheader")
-st.caption("this is the caption")
-st.code("x=2021")
-st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+st.title ("2022 World Cup Stats")
+st.header("An analysis on the 2022 world cup")
+# st.markdown("this is the header")
+# st.subheader("this is the subheader")
+# st.caption("this is the caption")
+# st.code("x=2021")
+# st.latex(r''' a+a r^1+a r^2+a r^3 ''')
