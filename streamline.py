9c68bc94e4c8eb93e61c8eda94b3fe14dc7ecdea
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-25 20:49:54 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 st.title ("2022 World Cup Stats")
-new_title = '<p style="font-family:Arial; color:Blue; font-size: 42px;">2022 World Cup Stats</p>'
+new_title = '<p style="font-family:sans-serif; color:Blue; font-size: 42px;">2022 World Cup Stats</p>'
 st.markdown(new_title, unsafe_allow_html=True)
 st.header("An analysis on the 2022 world cup")
 st.image("messi.jpg")
