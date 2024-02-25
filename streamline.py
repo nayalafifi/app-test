62e4930f12df712261696867405188cdf83b94dc
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-25 23:37:00 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -7,7 +7,6 @@ import streamlit as st
 backgroundColor = "#add8e6"
 st.title("2022 World Cup Stats")
 st.header("An analysis on the 2022 world cup")
-st.image("messi.jpg")
 
 st.radio('Pick your favorite team',['Argentina','Brazil','France','Germany','Morocco','Spain'])
 st.selectbox('Pick your player of the tournament',['Messi','Mbappe','Ronaldo','Neymar','Hakimi','Di Maria','Bellingham','Modric'])
