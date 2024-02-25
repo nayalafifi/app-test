cbade73d15ad5c7abb76dd20a44534db1416ce5d
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-26 00:21:29 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -15,9 +15,9 @@ st.radio('What is your favorite genre?',['Comedy','Drama','Mystery','Crime'])
 df = pd.read_csv('taylor_swift_spotify.csv')
 df.head()
 df.columns
-sampled_df = df.sample(n=1000)
-sampled_df_10_columns = sampled_df.iloc[:, :10]
-sns.pairplot(sampled_df_10_columns)
+# sampled_df = df.sample(n=1000)
+# sampled_df_10_columns = sampled_df.iloc[:, :10]
+# sns.pairplot(sampled_df_10_columns)
 
 
 
