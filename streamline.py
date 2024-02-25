c6338bd7a020d93ed92cec797ccdfc770a709abb
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-25 20:54:23 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -5,9 +5,8 @@ st.header("An analysis on the 2022 world cup")
 st.image("messi.jpg")
 
 st.radio('Pick your favorite team',['Argentina','Brazil','France','Germany','Morocco','Spain'])
-st.selectbox('Pick your sportr',['Basketball','Football'])
-st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
-st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.selectbox('Pick your player of the tournament',['Messi','Mbappe','Ronaldo','Neymar','Hakimi','Di Maria','Bellingham','Modric'])
+st.select_slider('How would you rate this tournament? ', ['Bad', 'Good', 'Excellent'])
 
 
 
