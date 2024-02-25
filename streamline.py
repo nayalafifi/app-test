63146d3245b38e3e4233813da291d090af956f09
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-25 20:45:19 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 st.title ("2022 World Cup Stats")
 st.header("An analysis on the 2022 world cup")
 st.image("messi.jpg")
+
+rand=np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
