3cb3ff57106611e743eb659f9ce00c62e49b62e0
nayalafifi <144211643+nayalafifi@users.noreply.github.com>
2024-02-26 12:03:18 +0100

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-# import hvplot.pandas
 import streamlit as st
 
 st.title("Taylor Swift Spotify Popularity")
